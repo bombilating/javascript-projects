8427fa6fed32ca562e6ce64fd714cc9ad21c2b3a
Jeremy <thisismejeremy@gmail.com>
2024-01-11 19:03:36 -0500

    booleans-and-conditional exercise

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -14,3 +14,25 @@ if (engineIndicatorLight === "green") {
 } else {
    console.log("engines are off");
 }
+
+if (crewStatus === true) {
+   console.log("Crew Ready");
+} else {
+   console.log("Crew Not Ready");
+}
+
+if (computerStatusCode === 200) {
+   console.log("Please stand by. Computer is rebooting");
+} else if (computerStatusCode === 400) {
+   console.log("Success! Computer online.");
+} else {
+   console.log("ALERT: Computer offline!");
+}
+
+if (shuttleSpeed > 17500) {
+   console.log("ALERT: Escape velocity reached!")
+} else if (shuttleSpeed < 8000) {
+   console.log("ALERT: Cannot maintain orbit!")
+} else {
+   console.log("Stable speed")
+}
