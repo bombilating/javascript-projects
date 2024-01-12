ccb382bfc8f6fdd98ea495e72ed30e02b7a3d4d8
Jeremy <thisismejeremy@gmail.com>
2024-01-11 21:39:23 -0500

    Class 2 Studio

--- booleans-and-conditionals/exercises/part-1.js
+++ booleans-and-conditionals/exercises/part-1.js
@@ -1,5 +1,6 @@
 // Declare and initialize the variables for exercise 1 here:
 
+
 // BEFORE running the code, predict what will be printed to the console by the following statements:
 
 if (engineIndicatorLight === "green") {
