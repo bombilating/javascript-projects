898455c9441078930c654958aab0ab45b140bb30
Jeremy <thisismejeremy@gmail.com>
2024-02-08 21:48:37 -0500

    unit testing studio

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,46 @@
 
 let launchcode = {
-  
+  organization: "nonprofit",
+  executiveDirector: "Jeff",
+  percentageCoolEmployees: 100,
+  programsOffered: ["Web Development", "Data Analysis", "Liftoff"]
 }
 
-module.exports = launchcode;
+function launchOutput(num) {
+    let statement = '';
+    if(num%2 != 0 && num%3 != 0 && num%5 != 0) {
+        statement = "Rutabagas! That doesn't work.";
+        return statement
+    }
+    if((num%2 == 0 || num%3 == 0) && num%5 == 0) {
+        if(num%3 != 0) {
+            statement = "Launch Rocks! (CRASH!!!!)"
+            return statement
+        }
+        if(num%2 == 0) {
+            statement += 'Launch';
+        }
+        if(num%3 == 0) {
+            statement += 'Code';
+        } 
+        statement += ' Rocks!';
+        return statement
+    }
+    if(num%2 == 0) {
+        statement += 'Launch';
+    }
+    if(num%3 == 0) {
+        statement += 'Code';
+    } 
+    if(num%5 == 0) {
+        statement += 'Rocks';
+    }
+    statement += "!"
+    return statement
+}
+
+module.exports = {
+    launchcode: launchcode,
+    launchOutput: launchOutput
+};
 
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -4,5 +4,60 @@ const launchcode = require('../index.js');
 describe("Testing launchcode", function(){
 
   // Write your unit tests here!
-  
+  // launchcode object properties
+  test("key value of 'organization' should return 'nonprofit'", function() {
+    expect(launchcode.launchcode["organization"]).toBe('nonprofit');
+  });
+
+  test("key value of 'executiveDirector' should return 'Jeff' in the object launchcode", function() {
+    expect(launchcode.launchcode["executiveDirector"]).toBe('Jeff');
+  })
+
+  test("key value of 'percentageCoolEmployees' should return '100'", function() {
+    expect(launchcode.launchcode["percentageCoolEmployees"]).toBe(100);
+  })
+
+  test("key value of 'programsOffered' should return an array of ['Web Development', 'Data Analysis', 'Liftoff']", function() {
+    expect(launchcode.launchcode["programsOffered"][0]).toBe('Web Development');
+    expect(launchcode.launchcode["programsOffered"][1]).toBe('Data Analysis');
+    expect(launchcode.launchcode["programsOffered"][2]).toBe('Liftoff');
+    expect(launchcode.launchcode["programsOffered"].length).toBe(3)
+  })
+
+  test("key value of 'organization' should return 'nonprofit'", function() {
+    expect(launchcode.launchcode["organization"]).toBe('nonprofit');
+  });
+
+  //launchOutput() function
+  test("When passed a number that is ONLY divisible by 2, launchOutput() returns 'Launch!''", function() {
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+  })
+
+  test("When passed a number that is ONLY divisible by 3, launchOutput() returns 'Code!'", function() {
+    expect(launchcode.launchOutput(3)).toBe('Code!');
+  })
+
+  test("When passed a number that is ONLY divisible by 5, launchOutput() returns 'Rocks!'", function() {
+    expect(launchcode.launchOutput(5)).toBe("Rocks!");
+  })
+
+  test("When passed a number that is divisible by 2 AND 3, launchOutput() returns 'LaunchCode!' (not 'Launch!Code!').", function() {
+    expect(launchcode.launchOutput(6)).toBe("LaunchCode!");
+  })
+
+  test("When passed a number that is divisible by 3 AND 5, launchOutput() return 'Code Rocks!'", function() {
+    expect(launchcode.launchOutput(15)).toBe("Code Rocks!");
+  })
+
+  test("When passed a number that is divisible by 2 AND 5, launchOutput() return 'Launch Rocks! (CRASH!!!!)'", function() {
+    expect(launchcode.launchOutput(10)).toBe("Launch Rocks! (CRASH!!!!)");
+  })
+
+  test("When passed a number that is divisible by 2, 3, AND 5, launchOutput() return 'LaunchCode Rocks!'", function() {
+    expect(launchcode.launchOutput(30)).toBe("LaunchCode Rocks!");
+  })
+
+  test("When passed a number that is NOT divisible by 2, 3, or 5, launchOutput() return 'Rutabagas! That doesn't work.'", function() {
+    expect(launchcode.launchOutput(7)).toBe("Rutabagas! That doesn't work.")
+  })
 });
